cbd96002b43dac5e28ab04f17d17e1259a5c78f6
charles delannoy <charles.delannoy@hei.fr>
2020-10-21 12:15:59 +0200

    creation of model, api class and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+puts 'destroying datas...'
+Review.destroy_all
+Airline.destroy_all
+
+puts 'creating datas...'
 airlines = Airline.create([
   {
     name: "United Airlines",
@@ -42,3 +47,5 @@ reviews = Review.create([
     airline: airlines.first
   }
 ])
+
+puts 'SUCCESS'
