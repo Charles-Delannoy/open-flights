2a2fd9088f967698720af3cefcf3dcafe765f015
charles delannoy <charles.delannoy@hei.fr>
2020-11-02 12:10:46 +0100

    enhanced form style

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -8,9 +8,11 @@ const RatingContainer = styled.div`
   text-align: center;
   border-radius: 4px;
   font-size: 18px;
-  padding: 40px 0 10px 0;
   border: 1px solid #e6e6e6;
   background: #fff;
+  margin: 0  18px 12px 18px;
+  padding: 12px;
+  width: calc(96% - 18px);
 `;
 
 const RatingBox = styled.div`
@@ -48,14 +50,16 @@ const RatingBox = styled.div`
 
 const Field = styled.div`
   border-radius: 4px;
+  padding: 10px;
+  margin: 0 auto;
 
   input {
     min-height: 50px;
     border-radius: 4px;
     border: 1px solid #e6e6e6;
-    margin: 0  0 12px 0;
+    margin: 0  18px 12px 18px;
     padding: 12px;
-    width: 96%;
+    width: calc(96% - 18px);
   }
 
   textarea {
@@ -78,15 +82,15 @@ const Wrapper = styled.div`
 
 const SubmitBtn = styled.button`
  color: #fff;
- background: #333;
+ background: #589d08;
  border-radius: 4px;
- padding: 12Px;
+ padding: 12px;
  font-size: 18px;
  cursor: pointer;
  transition: ease-in-out 0.1s;
- border: 1px solid #fff;
- width: 100%;
- margin-top: 20px;
+ border: 1px solid #589d08;
+ width: calc(96% - 18px);
+ margin: 20px 30px 0 30px;
 
  &:hover {
   background: #fff;
@@ -97,7 +101,7 @@ const SubmitBtn = styled.button`
 
 const Headline = styled.div`
   padding: 30px;
-  font-size: 20px;
+  font-size: 23px;
   font-weight: bold;
   color: #fff;
 `;
